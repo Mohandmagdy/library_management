dc08b4b0ffae9d752a62b43b38f0d3c550f64799
Mohandmagdy <mohandmagdii45@gmail.com>
2024-02-12 23:00:49 +0200

    books can now be filtered accoring to category, publisher or the author

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pool = require('../database/connection');
 
 const show_books = (req, res) => {
-    const myQuery = "select books.isbn, title, description, pieces as remaining_pieces, concat(firstName, ' ', lastName) as author_name from books inner join book_author on books.ISBN = book_author.isbn inner join authors on book_author.author_id = authors.author_id";
+    const myQuery = "select b.isbn, title, description, pieces, group_concat(Distinct concat(firstName, ' ', lastName)) as authors, group_concat(Distinct category) as categories from books as b inner join book_author as ba on b.ISBN = ba.isbn  inner join authors as a on ba.author_id = a.author_id inner join book_category as bc on bc.isbn = b.isbn group by b.isbn;";
     pool.query(myQuery, (err, result) => {
         if(err){
             console.log(err);
@@ -15,7 +15,13 @@ const show_books = (req, res) => {
 const show_book = (req, res) => {
     const id = req.params.id;
 
-    const myQuery = `select b.isbn, title, description, pages, pieces as remaining_copies, concat(firstName, ' ', lastName) as author_name, name as publisher_name, publish_year from books as b inner join book_author as ba on b.isbn = ba.isbn inner join authors as a on a.author_id = ba.author_id inner join publishers as p on b.publisher_id = p.publisher_id where b.isbn = ${id}`
+    const myQuery = `SELECT b.ISBN, b.title,  b.description,  b.pieces, p.name AS publisher_name, b.pages, b.publish_year, GROUP_CONCAT(DISTINCT CONCAT(a.firstName, ' ', a.lastName)) AS authors, GROUP_CONCAT(DISTINCT bc.category) AS categories
+            FROM books AS b
+            INNER JOIN book_author AS ba ON b.ISBN = ba.ISBN
+            INNER JOIN authors AS a ON ba.author_id = a.author_id
+            INNER JOIN book_category AS bc ON b.ISBN = bc.ISBN
+            INNER JOIN publishers AS p ON b.publisher_id = p.publisher_id
+            GROUP BY b.ISBN, b.title, b.description, b.pieces, b.publisher_id, p.name, b.pages, b.publish_year;`
     pool.query(myQuery, (err, result) => {
         if(err){
             console.log(err);
@@ -62,10 +68,10 @@ const add_book = (req, res) => {
             isbn = result.insertId;
 
             authors.forEach(authorId => {
-                const sql = 'INSERT INTO book_author (isbn, author_id) VALUES (?, ?)';
+                const myQuery2 = 'INSERT INTO book_author (isbn, author_id) VALUES (?, ?)';
                 const values = [isbn, authorId];
         
-                pool.query(sql, values, (err, result) => {
+                pool.query(myQuery2, values, (err, result) => {
                     if (err) {
                         pool.rollback(() => {
                             console.error('Error inserting record into book_author:', err);
@@ -76,10 +82,10 @@ const add_book = (req, res) => {
             });
 
             categories.forEach(categoryName => {
-                const sql = 'INSERT INTO book_category (ISBN, category) VALUES (?, ?)';
+                const myQuery3 = 'INSERT INTO book_category (ISBN, category) VALUES (?, ?)';
                 const values = [isbn, categoryName];
     
-                pool.query(sql, values, (err, result) => {
+                pool.query(myQuery3, values, (err, result) => {
                     if (err) {
                         pool.rollback(() => {
                             console.error('Error inserting record into book_category:', err);
@@ -103,7 +109,13 @@ const add_book = (req, res) => {
 }
 
 const filter_category = (category, res) => {
-    const myQuery = `select * from books as b inner join book_category as bc on b.isbn = bc.isbn and category = '${category}'`
+    const myQuery = `SELECT b.ISBN, b.title, b.description, b.pieces, b.pages, GROUP_CONCAT(DISTINCT concat(firstName, ' ', lastName)) AS authors, group_concat(Distinct bc.category) as categories
+            FROM books AS b
+            INNER JOIN book_author AS ba ON b.ISBN = ba.ISBN
+            INNER JOIN authors as a on a.author_id = ba.author_id
+            INNER JOIN book_category AS bc ON b.ISBN = bc.ISBN
+            GROUP BY b.ISBN
+            HAVING categories LIKE '%comedy%';`
     pool.query(myQuery, (err, result) => {
         if (err) {
             console.log(err);
@@ -114,7 +126,13 @@ const filter_category = (category, res) => {
 }
 
 const filter_publisher = (publisher, res) => {
-    const myQuery = `select * from books as b inner join book_category as bc on b.isbn = bc.isbn and category = '${category}'`
+    const myQuery = `SELECT b.ISBN, b.title, b.description, b.pieces, b.pages, GROUP_CONCAT(DISTINCT concat(firstName, ' ', lastName)) AS authors, group_concat(Distinct bc.category) as categories
+            FROM books AS b
+            INNER JOIN book_author AS ba ON b.ISBN = ba.ISBN
+            INNER JOIN authors as a on a.author_id = ba.author_id
+            INNER JOIN book_category AS bc ON b.ISBN = bc.ISBN
+            INNER JOIN publishers as p ON p.publisher_id = b.publisher_id and p.name = '${publisher}'
+            GROUP BY b.ISBN;`
     pool.query(myQuery, (err, result) => {
         if (err) {
             console.log(err);
@@ -125,7 +143,12 @@ const filter_publisher = (publisher, res) => {
 }
 
 const filter_author = (author, res) => {
-    const myQuery = `select * from books as b inner join book_category as bc on b.isbn = bc.isbn and category = '${category}'`
+    const myQuery = `SELECT b.ISBN, b.title, b.description, b.pieces, b.pages, GROUP_CONCAT(DISTINCT concat(firstName, ' ', lastName)) AS authors, group_concat(Distinct bc.category) as categories
+            FROM books AS b
+            INNER JOIN book_author AS ba ON b.ISBN = ba.ISBN
+            INNER JOIN authors as a on a.author_id = ba.author_id and concat(firstName, ' ', lastName) = '${author}'
+            INNER JOIN book_category AS bc ON b.ISBN = bc.ISBN
+            GROUP BY b.ISBN;`
     pool.query(myQuery, (err, result) => {
         if (err) {
             console.log(err);
@@ -136,7 +159,9 @@ const filter_author = (author, res) => {
 }
 
 const filter = (req, res) => {
+    
     const type = req.params.type;
+    
 
     if(type == 'author'){
         filter_author(req.body.author, res);
